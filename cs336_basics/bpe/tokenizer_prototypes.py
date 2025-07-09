76d453c449443b57c3a6170a683633b89bb26938
Mystri <boyouhan@gmail.com>
2025-07-10 01:40:59 +0800

    Completed Prototype BPE: Passed simple case in simple_text.txt

--- cs336_basics/bpe/tokenizer_prototypes.py
+++ cs336_basics/bpe/tokenizer_prototypes.py
@@ -2,7 +2,7 @@ import regex as re
 import os
 
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-EOT = '<|endoftext|>'
+EOT = b'<|endoftext|>'
 STARTER_VOCABULARY = [EOT] + [bytes([i]) for i in range(256)]
 
 def createTokenizer(input_path:str, vocab_size:int, special_tokens:list[str]=[]) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
@@ -26,39 +26,56 @@ def pretokenize_dummy_tuple_bytes(input_str: str) -> dict[tuple[bytes], int]:
   return frequency_table
 
 # Add all byte pairs of a word to a frequency table. For example, " lower" -> ' l', 'lo', 'ow', 'we', 'er'.
-def add_word_to_token_pair_freq_table(merge_freq_table, word):
+def add_word_to_token_pair_freq_table(merge_freq_table, word, word_freq):
   for i in range(len(word) - 1):
     pair = word[i:i + 2] # Get the pair
-    merge_freq_table[pair] = merge_freq_table.get(pair, 0) + 1
+    merge_freq_table[pair] = merge_freq_table.get(pair, 0) + word_freq
 
 # Dummy merge.
-def merge_dummy_naive(frequency_table: dict[tuple[bytes], int], steps: int):
-  token_pair_freq_table = {}
-
-  # Initialize the merge freq table by adding the byte pairs.
-  for word in frequency_table.keys():
-    add_word_to_token_pair_freq_table(token_pair_freq_table, word)
-
+def merge_dummy(tokenization_table: dict[tuple[bytes], int], steps: int) -> list[bytes]:
+  new_words = set()
+  merge_sequence = []
   for _ in range(steps):
+    token_pair_freq_table = {}
+    # Initialize the merge freq table by adding the byte pairs.
+    for word in tokenization_table.keys():
+      add_word_to_token_pair_freq_table(token_pair_freq_table, word, tokenization_table[word])
+    
     # Find the largest entry by frequency, break ties by lexicographical value.
-
+  
     # Naive implementation, using Python max.
     most_frequent_pair = max(token_pair_freq_table, key=lambda k: (token_pair_freq_table[k], k))
-    for word in frequency_table.keys():
+    merged_most_frequent_pair = most_frequent_pair[0] + most_frequent_pair[1]
+    # store words. to be optimized by indices
+    words_need_merge = []
+    for word in tokenization_table.keys():
+      # Store the indices of the pair with max frequency, instead of doing the merge in-place.
       for i in range(len(word) - 1):
         if word[i] == most_frequent_pair[0] and word[i + 1] ==  most_frequent_pair[1]:
-          print(f'Merging word {word[i:i + 2]}')
-          merged_token = word[i] + word[i + 1]
-          merged_word = word[:i] + tuple(merged_token) + word[i:]
-          frequency_table[merged_word] = frequency_table[word]
-          frequency_table[word] = -1
-          
+          print(f'Recording token merge for {word[i:i + 2]}, at position {i}')
+          words_need_merge.append(word)
+    
+    # Merge the words.
+    for word in words_need_merge:
+      for i in range(len(word) - 1):
+        if word[i] == most_frequent_pair[0] and word[i + 1] ==  most_frequent_pair[1]:
+          print(f'Recording token merge for {word[i:i + 2]}, for word {word}')
+          merged_word = (*word[:i], merged_most_frequent_pair, *word[i + 2:])
+          new_words.add(merged_most_frequent_pair)
+          tokenization_table[merged_word] = tokenization_table.pop(word)
+    print(f"Table after {steps} merge: {tokenization_table}")
+
+  return (list(new_words), merge_sequence)
+
+
 # Sanity check for the dummy pretokenization/merge.
 with open(rf'{os.path.dirname(os.path.abspath(__file__))}\\simple_text.txt', 'r', encoding='utf-8') as file:
   content = file.read()
   pretokenization = pretokenize_dummy_tuple_bytes(content)
   print(f'Dummy Pretokenization result: ${pretokenization}')
-  merge_dummy_naive(pretokenization, 1)
+  new_words = merge_dummy(pretokenization, 6)
+  vocab = STARTER_VOCABULARY + new_words
+  print(f'Merge result - New words: {new_words}')
 
 def merge_dummy():
   pass
