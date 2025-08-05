cfa7bffb4a66c5522b4ebaa9d3dde1b6f2f22dbb
Mystri <boyouhan@gmail.com>
2025-08-06 03:54:45 +0800

    Improved merge operations to pass run_train_bpe test

--- cs336_basics/bpe/multiprocessing_prototype.py
+++ cs336_basics/bpe/multiprocessing_prototype.py
@@ -25,7 +25,7 @@ def pre_tokenize_and_count(
     special_tokens = set(special_token_vocabulary.keys())
 
     words_list: list[Word] = []
-    
+
     # Split the text by the special tokens.
     if special_tokens_regex:
         pieces = special_tokens_regex.split(text)
@@ -52,7 +52,9 @@ def pre_tokenize_and_count(
 
 
 class PQEntry:
-    def __init__(self, first: Token, second: Token, first_bytes, second_bytes, frequency: int):
+    def __init__(
+        self, first: Token, second: Token, first_bytes, second_bytes, frequency: int
+    ):
         self.first = first
         self.second = second
         self.first_bytes = first_bytes
@@ -63,7 +65,10 @@ class PQEntry:
         # Compare by frequency first, then lexicographically by the pairs.
         # Uses greaterthan for max-heap behavior.
         if self.frequency == other.frequency:
-            return (self.first_bytes, self.second_bytes) > (other.first_bytes, other.second_bytes)
+            return (self.first_bytes, self.second_bytes) > (
+                other.first_bytes,
+                other.second_bytes,
+            )
         return self.frequency > other.frequency
 
     def get_merged_bytes(self, vocab: dict[int, bytes]) -> bytes:
@@ -71,7 +76,7 @@ class PQEntry:
 
     def get_merged_pair_bytes(self, vocab: dict[int, bytes]) -> tuple[bytes, bytes]:
         return (vocab[self.first], vocab[self.second])
-    
+
     def get_token_pair(self) -> tuple[Token, Token]:
         return (self.first, self.second)
 
@@ -89,9 +94,9 @@ class TokenPairPQ:
 
     def is_empty(self) -> bool:
         return len(self.pq) == 0
-    
 
-class WordFreq: 
+
+class WordFreq:
     def __init__(self, freq: int):
         self.freq = freq
 
@@ -120,10 +125,10 @@ def initialize_word(
         cursor.next = new_node
         new_node.prev = cursor
         cursor = new_node
-    
+
     dummy_node.next.prev = None  # Remove the reference of 1st word node pointing to dummy node after creation.
     cursor = dummy_node.next
-    
+
     # Traverse each pair in the word.
     while cursor and cursor.next:
         token_pair = cursor.token, cursor.next.token
@@ -150,7 +155,9 @@ def merge(
     token_pair_freq_table: dict[tuple[Token, Token], int] = defaultdict(int)
 
     # Create a map from each pair to their origins, so that each pair can refer back to the words that contained it.
-    pair_occurrences_in_words: dict[tuple[Token, Token], set[WordNode]] = defaultdict(set)
+    pair_occurrences_in_words: dict[tuple[Token, Token], set[WordNode]] = defaultdict(
+        set
+    )
 
     # Initialize the merge freq table by doing 2 things:
     # 1. count the pair by adding the word frequency to token_pair_freq_table.
@@ -160,6 +167,7 @@ def merge(
 
     token_pair_freq_table_ref = {}
     merge_freq_table = defaultdict(int)
+
     def add_word_to_token_pair_freq_table(merge_freq_table, word, word_freq):
         for i in range(len(word) - 1):
             pair = word[i : i + 2]  # Get the pair
@@ -173,11 +181,11 @@ def merge(
 
     # Convert the frequency table to a priority queue of TokenPair objects.
     token_pair_pq = TokenPairPQ(
-            [
-                PQEntry(pair[0], pair[1], vocab[pair[0]], vocab[pair[1]], freq)
-                for pair, freq in token_pair_freq_table.items()
-            ]
-        )
+        [
+            PQEntry(pair[0], pair[1], vocab[pair[0]], vocab[pair[1]], freq)
+            for pair, freq in token_pair_freq_table.items()
+        ]
+    )
 
     result_vocab = starter_vocab.copy()
 
@@ -192,12 +200,10 @@ def merge(
             # If the pair is not in the frequency table, it means it has been merged or is invalid.
             if token_pair not in token_pair_freq_table:
                 continue
-            
+
             # The frequency table stores the latest frequency of the pair.
             # If the pair's frequncy matches the table, then it is the latest state of the queue and we should merge it.
-            if (
-                entry.frequency == token_pair_freq_table[token_pair]
-            ):
+            if entry.frequency == token_pair_freq_table[token_pair]:
                 best_pq_entry = entry
                 break
         if best_pq_entry is None:
@@ -213,66 +219,80 @@ def merge(
 
         new_token = len(result_vocab) - 1  # The new token ID for the merged token.
 
-        pairs_with_updated_freq = set()        
+        pairs_with_updated_freq = set()
         # Do the merge operation for each occurrence of the pair.
-        for wordnode in pair_occurrences_in_words[most_frequent_pair].copy():  # Update the words that contain the most frequent pair.
+        for wordnode in pair_occurrences_in_words[
+            most_frequent_pair
+        ].copy():  # Update the words that contain the most frequent pair.
 
-            old_first, old_second = wordnode, wordnode.next
-            if not old_second:
+            left_node, right_node = wordnode, wordnode.next
+            if not right_node:
                 continue
-            old_token_pair = (old_first.token, old_second.token)
-            pair_occurrences_in_words[old_token_pair].discard(old_first)
+            old_token_pair = (left_node.token, right_node.token)
+            pair_occurrences_in_words[old_token_pair].discard(left_node)
 
-            # Create the new word node.
-            new_wordnode = WordNode(new_token, wordnode.word_freq)
-
-            if old_first.prev:
-                # 1. Modify the word.
-                old_first.prev.next = new_wordnode
-                new_wordnode.prev = old_first.prev
-
+            if left_node.prev:
                 # 2. Modify the map from pairs to words, since the word has changed.
                 # Remove the occurrence at the left of (old_first, old_second): (old_first.prev, old_first).
-                pair_occurrences_in_words[(old_first.prev.token, old_first.token)].discard(old_first.prev)
+                pair_occurrences_in_words[
+                    (left_node.prev.token, left_node.token)
+                ].discard(left_node.prev)
                 # Add the new occurrence: (old_first.prev, new_token).
-                pair_occurrences_in_words[(old_first.prev.token, new_token)].add(old_first.prev)
+                pair_occurrences_in_words[(left_node.prev.token, new_token)].add(
+                    left_node.prev
+                )
 
                 # 3. Modify the pair frequency table.
                 # Reduce the frequency of the word from the old pair.
-                token_pair_freq_table[(old_first.prev.token, old_first.token)] -= wordnode.word_freq.freq 
-                pairs_with_updated_freq.add((old_first.prev.token, new_token))
+                token_pair_freq_table[
+                    (left_node.prev.token, left_node.token)
+                ] -= wordnode.word_freq.freq
+                pairs_with_updated_freq.add((left_node.prev.token, left_node.token))
+
                 # Increase the frequency of the word in the new pair, also update the new in the .
-                token_pair_freq_table[old_first.prev.token, new_token] += wordnode.word_freq.freq
-                pairs_with_updated_freq.add((old_first.prev.token, new_token))
+                token_pair_freq_table[
+                    (left_node.prev.token, new_token)
+                ] += wordnode.word_freq.freq
+                pairs_with_updated_freq.add((left_node.prev.token, new_token))
 
-            if old_second.next:
-                # 1. Modify the word.
-                old_second.next.prev = new_wordnode
-                new_wordnode.next = old_second.prev
-
+            if right_node.next:
                 # 2. Modify the map from pairs to words, since the word has changed.
                 # Remove the occurrence at the right of (old_first, old_second): (old_second, old_second.next).
-                pair_occurrences_in_words[(old_second.token, old_second.next.token)].discard(old_second)
+                pair_occurrences_in_words[
+                    (right_node.token, right_node.next.token)
+                ].discard(right_node)
                 # Add the new occurrence: (old_first.prev, new_token).
-                pair_occurrences_in_words[(new_token, old_second.next.token)].add(new_wordnode)
+                pair_occurrences_in_words[(new_token, right_node.next.token)].add(
+                    left_node
+                )
 
-                # 3. Modify the pair frequency table.
+                # 3. Modify the pair frequency table, and add the changes to the frequency table.
                 # Reduce the frequency of the word from the old pair.
-                token_pair_freq_table[(old_second.token, old_second.next.token)] -= wordnode.word_freq.freq 
-                pairs_with_updated_freq.add((new_token, old_second.next.token))
+                token_pair_freq_table[
+                    (right_node.token, right_node.next.token)
+                ] -= wordnode.word_freq.freq
+                pairs_with_updated_freq.add((right_node.token, right_node.next.token))
                 # Increase the frequency of the word in the new pair.
-                token_pair_freq_table[(new_token, old_second.next.token)] += wordnode.word_freq.freq
+                token_pair_freq_table[
+                    (new_token, right_node.next.token)
+                ] += wordnode.word_freq.freq
+                pairs_with_updated_freq.add((new_token, right_node.next.token))
 
-                # 4. Add the new token pair and its frequency to the priority queue.
-                pairs_with_updated_freq.add((new_token, old_second.next.token))
-        
+            # Update the word.
+            left_node.token = new_token
+            left_node.next = right_node.next  # Skip the right node.
+            if right_node.next:
+                right_node.next.prev = left_node
+
         # Apply the updated frequencies to the priority queue.
         for pair in pairs_with_updated_freq:
             token_pair_pq.push(
                 PQEntry(
-                    pair[0], pair[1],
-                    result_vocab[pair[0]], result_vocab[pair[1]],
-                    token_pair_freq_table[pair]
+                    pair[0],
+                    pair[1],
+                    result_vocab[pair[0]],
+                    result_vocab[pair[1]],
+                    token_pair_freq_table[pair],
                 )
             )
 
@@ -282,68 +302,68 @@ def merge(
 
     return (result_vocab, merge_sequence)
 
+if __name__ == "__main__":
+    # Initialization
+    start_time = time.time()
+    vocab: Vocab = {i: bytes([i]) for i in range(256)}
+    PAT = r"'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"
+    compiled_PAT = re.compile(PAT)
+    file = "tests/fixtures/tinystories_sample.txt"
 
-# Initialization
-start_time = time.time()
-vocab: Vocab = {i: bytes([i]) for i in range(256)}
-PAT = r"'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"
-compiled_PAT = re.compile(PAT)
-file = "tests/fixtures/tinystories_sample.txt"
+    special_tokens = ["<|endoftext|>"]
+    # Add special tokens to vocabulary.
+    # Also create a mapping from special token to its ID.
+    special_token_reference_vocabulary = {}
+    for token in special_tokens:
+        token_bytes = token.encode("utf-8")
+        if token_bytes not in vocab.values():
+            vocab[len(vocab)] = token_bytes
+            special_token_reference_vocabulary[token] = len(vocab) - 1
 
-special_tokens = ["<|endoftext|>"]
-# Add special tokens to vocabulary.
-# Also create a mapping from special token to its ID.
-special_token_reference_vocabulary = {}
-for token in special_tokens:
-    token_bytes = token.encode("utf-8")
-    if token_bytes not in vocab.values():
-        vocab[len(vocab)] = token_bytes
-        special_token_reference_vocabulary[token] = len(vocab) - 1
-
-# Create a splitter of text, by the special tokens
-special_tokens_sorted = sorted(
-    [t.encode("utf-8") for t in special_tokens], key=len, reverse=True
-)
-escaped_tokens = [re.escape(t.decode("utf-8")) for t in special_tokens_sorted]
-special_tokens_regex = "|".join(escaped_tokens)
-if special_tokens_regex:
-    special_token_pattern = re.compile(f"({special_tokens_regex})")
-
-# Create a reference vocabulary for special tokens
-special_token_to_id = {token: i for i, token in enumerate(special_tokens)}
-
-tasks = []
-with open(file, "rb") as f:
-    before_pretokenization_time = time.time()
-
-    boundaries = find_chunk_boundaries(
-        f, num_processes, "<|endoftext|>".encode("utf-8")
+    # Create a splitter of text, by the special tokens
+    special_tokens_sorted = sorted(
+        [t.encode("utf-8") for t in special_tokens], key=len, reverse=True
     )
+    escaped_tokens = [re.escape(t.decode("utf-8")) for t in special_tokens_sorted]
+    special_tokens_regex = "|".join(escaped_tokens)
+    if special_tokens_regex:
+        special_token_pattern = re.compile(f"({special_tokens_regex})")
 
-    # Create a list of tasks, for each chunk.
-    for start, end in zip(boundaries[:-1], boundaries[1:]):
-        f.seek(start)
-        chunk = f.read(end - start).decode("utf-8", errors="ignore")
-        tasks.append(
-            (
-                chunk.encode("utf-8"),
-                special_token_reference_vocabulary,
-                special_token_pattern,
-                compiled_PAT,
-            )
+    # Create a reference vocabulary for special tokens
+    special_token_to_id = {token: i for i, token in enumerate(special_tokens)}
+
+    tasks = []
+    with open(file, "rb") as f:
+        before_pretokenization_time = time.time()
+
+        boundaries = find_chunk_boundaries(
+            f, num_processes, "<|endoftext|>".encode("utf-8")
         )
 
-print(f"Initialization time: {time.time() - start_time:.2f} seconds")
+        # Create a list of tasks, for each chunk.
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            tasks.append(
+                (
+                    chunk.encode("utf-8"),
+                    special_token_reference_vocabulary,
+                    special_token_pattern,
+                    compiled_PAT,
+                )
+            )
 
-# start_time = time.time()
-# # Simulates parallel pre-tokenization. todo: implement using multiprocessing.
-# word_freq_table = Counter()
-# for task in tasks:
-#     word_freq_table.update(pre_tokenize_and_count(task))
-# print(f"Pre-tokenization time: {time.time() - start_time:.2f} seconds")
+    print(f"Initialization time: {time.time() - start_time:.2f} seconds")
 
-# start_time = time.time()
+    start_time = time.time()
+    # Simulates parallel pre-tokenization. todo: implement using multiprocessing.
+    word_freq_table = Counter()
+    for task in tasks:
+        word_freq_table.update(pre_tokenize_and_count(task))
+    print(f"Pre-tokenization time: {time.time() - start_time:.2f} seconds")
 
-# # Merge, which is not parallelizable.
-# vocab, merges = merge(vocab, word_freq_table, steps=100)
-# print(f"Merging time: {time.time() - start_time:.2f} seconds")
+    start_time = time.time()
+
+    # Merge, which is not parallelizable.
+    vocab, merges = merge(vocab, word_freq_table, steps=100)
+    print(f"Merging time: {time.time() - start_time:.2f} seconds")
