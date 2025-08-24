2f4760e0dc7c433e019a2d8953252fde2cd5ff62
Mystri <boyouhan@gmail.com>
2025-08-24 22:05:08 +0800

    Added comments

--- cs336_basics/transformer/loss.py
+++ cs336_basics/transformer/loss.py
@@ -8,10 +8,14 @@ def cross_entropy(
     logits: Float[Tensor, " ... seq_len vocab_size"],
     dim: int = -1
 ) -> Float:
+    # Since it is the only event possible, because the training data is [0, 1, 0, 0] - onehot.
+    # so only the -log (p_logits), of the expected word.
     max_logits = torch.max(logits, dim=dim, keepdim=True).values
     shifted_logits = logits - max_logits
     # top = log(exp(logits[expected]) - exp(logits.max)) = logits[expected] - logits.max
+    # logits[expected]
     top = logits.gather(dim=dim, index=expected.unsqueeze(1))
+    # logits[expected] - logits.max
     top -= max_logits
 
     # bottom = log(sum exp(logits) over every words)
